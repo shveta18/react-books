8a22f308a9a21d66190f04314af13d35b3841072
Shveta Kumar <sk@shveta.ca>
2020-08-26 11:32:44 -0400

    changed the index.js file to include mongodb

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
+require("dotenv").config()
+
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
 
-// Define middleware here
+const PORT = process.env.PORT || 3001;
+const mongoLink = process.env.MONGO_KEY;
+const MONGODB_URI = process.env.MONGODB_URI || mongoLink;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+app.use(routes);
+mongoose.connect(MONGODB_URI);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+app.listen(PORT, function () {
+  console.log("Server listening on: http://localhost:" + PORT + "!");
 });
